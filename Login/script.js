d7396adc009a2610b481409d1bf0b113f39b0e66
Pranav Rayban <raybanpranav.mahesh2023@vitstudent.ac.in>
2025-03-05 12:36:59 +0530

    Update script.js

--- Login/script.js
+++ Login/script.js
@@ -1,4 +1,4 @@
-const apiBaseURL = "http://localhost:5000/api"; // Adjust when hosting
+const apiBaseURL = "https://vercel2-lost-found.vercel.app/api"; // Adjust when hosting
 console.log("data")
 const token = localStorage.getItem("token");
 
